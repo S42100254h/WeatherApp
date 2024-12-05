ea6a9c9ec9000d30bec7d78b32a2f44879dce901
yuki sawabe <sawabe0v0@gmail.com>
2024-12-05 21:25:02 +0900

    モデルを作成

---
+++ 7_Clima-iOS13/Clima/Model/RegionModel.swift
@@ -0,0 +1,15 @@
+//
+//  RegionModel.swift
+//  Clima
+//
+//  Created by Uki on 2024/11/26.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct RegionModel {
+    let name: String
+    let cityList: [String]
+    var isExpanded: Bool
+}
